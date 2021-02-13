c9bdbf2cc69a76e85536f69ad4f6a774b7983704
ibrahim.b.beltagy <ibrahim.b.beltagy@gmail.com>
2021-02-14 02:53:39 +0400

    Update HomeService.swift

--- JibSpaceX/JibSpaceX/Core/Services/HomeService.swift
+++ JibSpaceX/JibSpaceX/Core/Services/HomeService.swift
@@ -8,33 +8,37 @@
 
 import Foundation
 import Alamofire
+import ObjectMapper
 
 protocol HomeServiceProtocol {
     
     // MARK: - Home page items -
-    func getHomeItems(completion: @escaping (Dictionary<String, Any>?, Error?) -> Void)
-
+    func getHomeItems(completion: @escaping ([Launch]?, Error?) -> Void)
+    
 }
 
 class HomeService: HomeServiceProtocol {
     
-    func getHomeItems(completion: @escaping (Dictionary<String, Any>?, Error?) -> Void) {
+    func getHomeItems(completion: @escaping ([Launch]?, Error?) -> Void) {
         // check for internet connection / load cache before request
-
+        
         guard let url = URL(string: Constants.baseUrl + Endpoint.homeEndpoints.homeItemsEndpoint) else {
-                   return
+            return
         }
         
-        //  The api doesn’t specify what parameter to use for the pagination but it should be implemented if available, and it's advised to have pagination in cases of lots of entries (would need filtering as well in that case since not all the data would be available for local filtering)
-        
+        //  The provided api doesn’t specify what parameter to use for the pagination but it should be implemented if available, and it's advised to have pagination in cases of lots of entries (would need filtering as well in that case since not all the data would be available for local filtering)
         AF.request(url, method: HTTPMethod.get, parameters: [:], encoding: URLEncoding.default, headers: ["Content-Type": "application/json"]).responseJSON{ response in
+            
             switch response.result {
             case .success(let value):
-                guard let value = value as? Dictionary<String, Any> else {
+                guard let value = value as? [[String: Any]] else {
                     completion(nil, response.error)
                     return
                 }
-                completion(value, nil)
+                
+                let launches = Mapper<Launch>().mapArray(JSONArray: value)
+                completion(launches, nil)
+                
             case .failure:
                 completion(nil, response.error)
             }
