600a62b57f237564c40fdd4eb10f749d2f42c793
ibrahim.b.beltagy <ibrahim.b.beltagy@gmail.com>
2021-02-14 05:17:54 +0400

    Project update and activity indicator update

--- JibSpaceX/JibSpaceX/Helpers/Extensions/UITableView/UITableView+ActivityIndicator.swift
+++ JibSpaceX/JibSpaceX/Helpers/Extensions/UITableView/UITableView+ActivityIndicator.swift
@@ -12,7 +12,7 @@ extension UITableView {
     
     func showActivityIndicator() {
         DispatchQueue.main.async {
-            let activityView = UIActivityIndicatorView(style: .gray)
+            let activityView = UIActivityIndicatorView(style: .medium)
             self.backgroundView = activityView
             activityView.startAnimating()
         }
