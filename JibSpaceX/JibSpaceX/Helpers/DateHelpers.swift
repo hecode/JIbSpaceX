28016c3d9fb5f0ac506ac2dfbb1caa8fcc198100
ibrahim.b.beltagy <ibrahim.b.beltagy@gmail.com>
2021-02-14 05:15:22 +0400

    New date helper and format and documentation

--- JibSpaceX/JibSpaceX/Helpers/DateHelpers.swift
+++ JibSpaceX/JibSpaceX/Helpers/DateHelpers.swift
@@ -8,6 +8,14 @@
 
 import Foundation
 
+enum DateFormat {
+    static let yearMonthDayTime = "d MMM yyyy, h:mm a"
+}
+
+/// yearsBetweenDates
+/// - Parameters:
+///   - startDate: start date  for the difference calculation
+///   - endDate: end end for the difference calculation
 func yearsBetweenDates(startDate: Date?, endDate: Date?) -> Int {
     guard let startDate = startDate, let endDate = endDate else {
         return 0
@@ -17,4 +25,23 @@ func yearsBetweenDates(startDate: Date?, endDate: Date?) -> Int {
     let difference = calendar.dateComponents([.nanosecond], from: startDate, to: endDate)
 
     return difference.year ?? 0
+}
+
+/// getFormattedDate
+/// - Parameters:
+///   - date: Date?
+///   - format: the string format used for the output date string
+///   - locale: locale
+///   - timeZone: timezone
+func getFormattedDate(date: Date?, format: String, locale: Locale? = Locale.current, timeZone: TimeZone? = TimeZone.current) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = format
+    dateFormatter.locale = locale
+    dateFormatter.timeZone = timeZone
+    
+    if let date = date {
+        return dateFormatter.string(from: date)
+    } else {
+        return Constants.invalidDateFormat
+    }
 }
